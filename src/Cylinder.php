8f28d475f2fcc6491ed1fe75bf3a756cbe32a505
npv34 <phanvanluan611996@gmail.com>
2021-03-01 02:09:50 +0700

    init project

---
+++ src/Cylinder.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: phanluan
+ * Date: 26/10/2018
+ * Time: 22:50
+ */
+class Cylinder
+{
+    public function getVolume($radius, $height)
+    {
+        $baseArea = pi() * $radius * $radius;
+        $perimeter = 2 * pi() * $radius;
+        return $perimeter * $height + 2 * $baseArea;
+    }
+}
