ead184567f86eeefda0464b6fb07ad64a813a866
thanhdeptrai <thanh.n5151@gmail.com>
2021-05-13 21:03:00 +0700

    first

--- src/Cylinder.php
+++ src/Cylinder.php
@@ -4,6 +4,16 @@ class Cylinder
 {
     public function getVolume($radius, $height)
     {
+        return $this->extracted($radius, $height);
+    }
+
+    /**
+     * @param $radius
+     * @param $height
+     * @return float|int
+     */
+    public function extracted($radius, $height): int|float
+    {
         $baseArea = pi() * $radius * $radius;
         $perimeter = 2 * pi() * $radius;
         return $perimeter * $height + 2 * $baseArea;
